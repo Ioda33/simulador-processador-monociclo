dfc5fd6521a2cb56f3f3024dc394f6b85ee6d881
Iolanda Stange Goldoni <iolandastgold@gmail.com>
2026-07-07 20:51:39 -0300

    cria os registradores
    Implemented a class to manage registers with get and set methods.

--- Registradores.java
+++ Registradores.java
@@ -1,3 +1,17 @@
 public class Registradores {
-    // cria os registradores
+    public class Registradores {
+    private short[] r;
+    public Registradores(){
+        r = new short[8];
+    }
+
+    public short get(int indice) {
+        return r[indice & 0x7];
+        //precisamos fazer uma limitação, mostrar que dá erro, tanto aqui quanto na parte da memória
+    }
+
+    public void set(int indice, short value) {
+        r[indice & 0x7] = value;
+    }
+}
 }
