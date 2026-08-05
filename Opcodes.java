117d5e3f2284edfdfc2a32890773dfff7270772a
Arthur Ramo <20251pvai10030037@estudantes.ifpr.edu.br>
2026-08-04 21:44:52 -0300

    tava tudo errado

--- Opcodes.java
+++ Opcodes.java
@@ -3,14 +3,12 @@ public class Opcodes {
     // Formato R
     public static final int ADD = 0;
     public static final int SUB = 1;
-    public static final int AND = 2;
-    public static final int OR  = 3;
-    public static final int STORE = 4;
-    public static final int LOAD  = 5;
-    public static final int MUL = 6;
-    public static final int DIV = 7;
-    public static final int CMP_EQUAL = 8;
-    public static final int CMP_NEQ = 9;
+    public static final int MUL = 2;
+    public static final int DIV  = 3;
+    public static final int CMP_EQUAL = 4;
+    public static final int CMP_NEQ  = 5;
+    public static final int LOAD = 15;
+    public static final int STORE = 16;
     public static final int SYSCALL = 63;
 
     // Formato I
